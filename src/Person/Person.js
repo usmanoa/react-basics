8738712b1af8264b6797941d907a7d49417c1dfa
usmanoa <usmanakorede@gmail.com>
2019-10-04 03:05:06 +0100

    Created Person component

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Person.css'
+
+const  person = props => {
+    return (
+        <div className="person">
+            <p>My name is {props.name}</p>
+            <input type="text" value={props.value} onChange={props.change}/>
+            <p>I'm {props.age} years old</p>
+        </div>
+    )
+}
+
+export default person;
